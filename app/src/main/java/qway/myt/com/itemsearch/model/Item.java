48acf65d2af2e8b2f25a9312f4b8f712085b0b60
MytTantely <myt.tantely@gmail.com>
2019-03-20 12:13:36 -0600

    first commit

---
+++ app/src/main/java/qway/myt/com/itemsearch/model/Item.java
@@ -0,0 +1,95 @@
+package qway.myt.com.itemsearch.model;
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Item {
+    private String openLibraryId;
+    private String author;
+    private String title;
+
+    public String getOpenLibraryId() {
+        return openLibraryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // Get medium sized book cover from covers API
+    public String getCoverUrl() {
+        return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-M.jpg?default=false";
+    }
+
+    // Get large sized book cover from covers API
+    public String getLargeCoverUrl() {
+        return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-L.jpg?default=false";
+    }
+
+    // Returns a Book given the expected JSON
+    public static Item fromJson(JSONObject jsonObject) {
+        Item item = new Item();
+        try {
+            // Deserialize json into object fields
+            // Check if a cover edition is available
+            if (jsonObject.has("cover_edition_key"))  {
+                item.openLibraryId = jsonObject.getString("cover_edition_key");
+            } else if(jsonObject.has("edition_key")) {
+                final JSONArray ids = jsonObject.getJSONArray("edition_key");
+                item.openLibraryId = ids.getString(0);
+            }
+            item.title = jsonObject.has("title_suggest") ? jsonObject.getString("title_suggest") : "";
+            item.author = getAuthor(jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // Return new object
+        return item;
+    }
+
+    // Return comma separated author list when there is more than one author
+    private static String getAuthor(final JSONObject jsonObject) {
+        try {
+            final JSONArray authors = jsonObject.getJSONArray("author_name");
+            int numAuthors = authors.length();
+            final String[] authorStrings = new String[numAuthors];
+            for (int i = 0; i < numAuthors; ++i) {
+                authorStrings[i] = authors.getString(i);
+            }
+            return TextUtils.join(", ", authorStrings);
+        } catch (JSONException e) {
+            return "";
+        }
+    }
+
+    // Decodes array of book json results into business model objects
+    public static ArrayList<Item> fromJson(JSONArray jsonArray) {
+        ArrayList<Item> items = new ArrayList<Item>(jsonArray.length());
+        // Process each result in json array, decode and convert to business
+        // object
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject bookJson = null;
+            try {
+                bookJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+            Item book = Item.fromJson(bookJson);
+            if (book != null) {
+                items.add(book);
+            }
+        }
+        return items;
+    }
+}
---
+++ app/src/main/java/qway/myt/com/itemsearch/model/ItemClient.java
@@ -0,0 +1,30 @@
+package qway.myt.com.itemsearch.model;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class ItemClient {
+    private static final String API_BASE_URL = "http://openlibrary.org/";
+    private AsyncHttpClient client;
+
+    public ItemClient() {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl) {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // Method for accessing the search API
+    public void getItems(final String query, JsonHttpResponseHandler handler) {
+        try {
+            String url = getApiUrl("search.json?q=");
+            client.get(url + URLEncoder.encode(query, "utf-8"), handler);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}

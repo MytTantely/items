8d0e80d5a8787d69ab2710c5811ef88f86a367a9
MytTantely <myt.tantely@gmail.com>
2019-03-31 11:00:38 -0600

    click on marker text

--- app/src/main/java/qway/myt/com/itemsearch/MapsActivity.java
+++ app/src/main/java/qway/myt/com/itemsearch/MapsActivity.java
@@ -1,18 +1,32 @@
 package qway.myt.com.itemsearch;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import qway.myt.com.itemsearch.utils.PermissionUtils;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +53,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng evanston1 = new LatLng(51.172560, -114.109000);
+        mMap.addMarker(new MarkerOptions()
+                .position(evanston1)
+                .title("Sobeys - Evanston")
+                .snippet("Apple - royal gala: $5.99/lb")
+        );
+
+        LatLng evanston2 = new LatLng(51.161020, -114.064110);
+        mMap.addMarker(new MarkerOptions()
+                .position(evanston2)
+                .title("Superstore - Panorama Hills")
+                .snippet("Apple - royal gala: $3.99/lb")
+        );
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(evanston1)
+                .include(evanston2)
+                .build();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+        enableMyLocation();
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+
+            @Override
+            public void onInfoWindowClick(Marker arg0) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MapsActivity.this, R.string.app_name, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
     }
 }

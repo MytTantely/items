fe5cb335dba11ec8695e5c524ac9c85e8f83a621
MytQwayNode <myt.qway.node@gmail.com>
2019-05-06 19:55:39 -0600

    draft recyclerview...

--- app/src/main/java/qway/myt/com/itemsearch/SearchFragment.java
+++ app/src/main/java/qway/myt/com/itemsearch/SearchFragment.java
@@ -4,15 +4,40 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
+    private RecyclerView recList;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+
+
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+
+        recList = (RecyclerView) v.findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(v.getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        return v;
+
     }
 }
